a6cfd7a4037729a5b39caa444794f5e4034249f1
22hsantos <22hsantos@wakatipu.school.nz>
2025-03-19 14:24:37 +1300

    Create SNDBX.py

---
+++ SNDBX.py
@@ -0,0 +1,44 @@
+
+''' 
+This program allows the user to access 
+and modify student NCEA data through the menu.
+'''
+
+
+#main menu
+def menu():
+    while True:
+        menu_input = input(
+            '''
+NCEA Student Qualification Portal\n
+1. All Student Data\n
+2. All Passing Students\n
+3. All Endorsed Students\n
+4. Student Summary\n
+5. Adjust Credits\n
+6. Add new Student data.''').strip
+        
+        if menu_input == '1':
+            all_students()
+            break
+        if menu_input == '2':
+            passing_students()
+            break
+        if menu_input == '3':
+            endorsed_students()
+            break
+        if menu_input == '4':
+            summary_menu()
+            break
+        if menu_input == '5':
+            adjust_credit()
+            break
+        if menu_input == '6':
+            add_student_data()
+            break
+        else:
+            print('Please enter a valid input.')
+
+
+
+menu()
